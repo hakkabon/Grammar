4717dc1692ce6d256a5f331ec2b275694edbf786
Ulf Akerstedt-Inoue <hakkabon@gmail.com>
2024-01-08 17:18:05 +0900

    removing dependencies to Argument Parser

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,14 +9,15 @@ let package = Package(
         .library(name: "Grammar", targets: ["Grammar"])
     ],
     dependencies: [
-        .package(name: "Files", url: "https://github.com/johnsundell/files.git", from: "2.2.1"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
-        .package(name: "BNF", url: "https://github.com/hakkabon/BNF-Parser", from: "1.0.0"),
     ],
     targets: [
-        .target(name: "Grammar", dependencies: ["BNF", "Files"]),
-        .target(name: "grm", dependencies: ["Grammar", "Files",
-                .product(name: "ArgumentParser", package: "swift-argument-parser")]),
-        .testTarget(name: "GrammarTests", dependencies: ["Grammar"]),
+        .target(
+            name: "Grammar",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "GrammarTests",
+            dependencies: ["Grammar"]
+        ),
     ]
 )
